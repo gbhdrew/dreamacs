4eb822a72a390f311c7f427fabe2d093a0e51d27
gbhdrew <gbhdrew@posteo.de>
2020-08-29 22:37:21 -0400

    Better scroll behavior
    Switch to one line at a time

--- init.el
+++ init.el
@@ -65,6 +65,13 @@
 ;; open org-mode links in the same frame
 (setq org-link-frame-setup '((file . find-file)))
 
+;; scroll one line at a time (less "jumpy" than defaults)
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+(setq scroll-step 1) ;; keyboard scroll one line at a time
+
+
 ;; Don't edit below this line:
 ;; ---------------------------
 
